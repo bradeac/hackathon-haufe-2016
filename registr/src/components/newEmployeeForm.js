9b9e335a8aa91af2b0f407a0d21fec165ef09eaf
boco4ever <boco4ever@yahoo.com>
2016-10-21 13:22:48 +0300

    added button to employeeForm

--- registr/src/components/newEmployeeForm.js
+++ registr/src/components/newEmployeeForm.js
@@ -3,9 +3,10 @@ import {
     Form, InputField,
     PickerField, DatePickerField
 } from 'react-native-form-generator';
+import { Button, Card, CardSection, TextBox, Spinner } from './common';
 
 class NewEmployeeForm extends Component {
-    scope = {
+    state = {
         firstName: '',
         lastName: '',
         gender: '',
@@ -31,28 +32,38 @@ class NewEmployeeForm extends Component {
     }
     render() {
         return (
-            <Form
-                ref='registrationForm'
-                onFocus={this.handleFormChange.bind(this)}
-                onChange={this.handleFormChange.bind(this)}
-                label="Personal Information"
-            >
-                <InputField ref='firstName' placeholder='First Name' />
-                <InputField ref='lastName' placeholder='Last Name' />
-                <PickerField
-                    ref='gender'
-                    placeholder='Gender'
-                    options={{
-                        male: 'Male',
-                        female: 'Female'
-                    }}
-                />
-                <DatePickerField
-                    ref='birthday'
-                    minimumDate={new Date('1/1/1900')}
-                    maximumDate={new Date()} mode='date' placeholder='Birthday'
-                />
-            </Form>
+
+            <Card>
+                <Form
+                    ref='registrationForm'
+                    onFocus={this.handleFormChange.bind(this)}
+                    onChange={this.handleFormChange.bind(this)}
+                    label="Personal Information"
+                    >
+                    <InputField ref='firstName' placeholder='First Name' />
+                    <InputField ref='lastName' placeholder='Last Name' />
+                    <InputField ref='personalId' placeholder='Personal Id' />
+                    <InputField ref='adress' placeholder='Address' />
+                    <PickerField
+                        ref='gender'
+                        placeholder='Gender'
+                        options={{
+                            male: 'Male',
+                            female: 'Female'
+                        }}
+                        />
+                    <DatePickerField
+                        ref='birthday'
+                        minimumDate={new Date('1/1/1900')}
+                        maximumDate={new Date()} mode='date' placeholder='Birth date'
+                        />
+                </Form>
+                <CardSection>
+                    <Button>
+                        Save
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
