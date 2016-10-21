d91224eddf3383e0f04b4e59f904d9736796596a
boco4ever <boco4ever@yahoo.com>
2016-10-21 19:40:40 +0300

    cleared data

--- registr/src/components/newEmployeeForm.js
+++ registr/src/components/newEmployeeForm.js
@@ -6,14 +6,12 @@ const Item = Picker.Item;
 
 class NewEmployeeForm extends Component {
     state = {
-        formData: {
-            firstName: '',
-            lastName: '',
-            gender: '',
-            personalId: '',
-            adress: '',
-            birthday: '2016-10-21'
-        },
+        firstName: '',
+        lastName: '',
+        gender: '',
+        personalId: '',
+        adress: '',
+        birthday: '2016-10-21',
         loading: false,
         error: ''
     };
@@ -27,28 +25,31 @@ class NewEmployeeForm extends Component {
             }
             console.log(employees);
             AsyncStorage.setItem('employees', JSON.stringify(employees))
-                .then(() => {
-                    this.setState({
-                        formData: {
-                            firstName: '',
-                            lastName: '',
-                            gender: '',
-                            personalId: '',
-                            adress: '',
-                            birthday: ''
-                        },
-                        loading: false
-                    });
-                    AsyncStorage.getItem('employees').then((value2) => {
-                        console.log(value2);
-                    }).done();
-                }).catch(error => {
-                    this.setState({
-                        loading: false,
-                        error: `An error occuerd! ${error}`
-                    });
-                });
+                .then(this.onSaveSuccess.bind(this))
+                .catch(this.onSaveFail.bind(this));
         }).done();
+    }
+
+    onSaveSuccess() {
+        this.setState({
+            firstName: '',
+            lastName: '',
+            gender: '',
+            personalId: '',
+            adress: '',
+            birthday: '2016-10-21',
+            loading: false
+        });
+        AsyncStorage.getItem('employees').then((value2) => {
+            console.log(value2);
+        }).done();
+    }
+
+    onSaveFail(error) {
+        this.setState({
+            loading: false,
+            error: `An error occuerd! ${error}`
+        });
     }
 
     handleFormChange(formData) {
@@ -72,53 +73,54 @@ class NewEmployeeForm extends Component {
                 <CardSection>
                     <TextBox
                         label={'First name'}
-                        onChangeText={firstName => this.setState({ formData: { firstName } })}
+                        onChangeText={firstName => this.setState({ firstName })}
                         placeholder={'eg. John'}
                         value={this.state.firstName}
-                    />
+                        />
                 </CardSection>
                 <CardSection>
                     <TextBox
                         label={'Last name'}
-                        onChangeText={lastName => this.setState({ formData: { lastName } })}
+                        onChangeText={lastName => this.setState({ lastName })}
                         placeholder={'eg. Snow'}
                         value={this.state.lastName}
-                    />
+                        />
                 </CardSection>
                 <CardSection>
                     <TextBox
                         label={'Personal id'}
-                        onChangeText={personalId => this.setState({ formData: { personalId } })}
+                        onChangeText={personalId => this.setState({ personalId })}
                         placeholder={'12345678'}
                         value={this.state.personalId}
-                    />
+                        />
                 </CardSection>
                 <CardSection>
                     <TextBox
                         label={'Address'}
-                        onChangeText={adress => this.setState({ formData: { adress } })}
+                        onChangeText={adress => this.setState({ adress })}
                         placeholder={'str. Address nr. 0'}
                         value={this.state.adress}
-                    />
+                        />
                 </CardSection>
-                 <CardSection>
+                <CardSection>
                     <Select
                         label={'Gender'}
-                        onValueChange={gender => this.setState({ formData: { gender } })}
+                        onValueChange={gender => this.setState({ gender })}
                         value={this.state.gender}
-                    >
+                        >
                         <Item label="Male" value="Male" />
                         <Item label="Female" value="Female" />
                     </Select>
                 </CardSection>
-                 <CardSection>
+                <CardSection>
                     <DateTime
                         label={'Birth date'}
-                        onDateChange={birthday => { this.setState({ formData: { birthday } });
-                        console.log(this.state.formData.birthday);
-                    }}
+                        onDateChange={birthday => {
+                            this.setState({ birthday });
+                            console.log(this.state.birthday);
+                        } }
                         value={this.state.birthday}
-                    />
+                        />
                 </CardSection>
                 <Text style={styles.errorStyle}>
                     {this.state.error}
